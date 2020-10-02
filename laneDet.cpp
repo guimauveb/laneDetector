7df511b3d0bcca410d1b3609b807530a2f059fbe
Guimauve <guillaume.bournel@gmail.com>
2020-10-02 16:35:48 +0200

    some clean up

--- laneDet.cpp
+++ laneDet.cpp
@@ -1,20 +1,5 @@
 /* Basic road lane detection program inspired by various project I found on the web, most of them written in 
- * Python. No ML / AI involved. 
- *
- *      TODO - Discard lines with an extreme slope (set the same limit as the ones 
- *      returned by Hough transform
- *
- *       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- *       ! Have 3 or 4 differents methods of recognizing the lanes according to the results !
- *       ! and conditions (weather, light...). If no lane is detected after a certain time  !    
- *       ! switch to another method.                                                        !
- *       ! (adaptive thresholding (darker scenes), canny edge on brighter scenes, what else ?                                                                       !
- *       ! Define the ROI according to src and road type !! 
- *       ! Set min width between lines detected !(approx width of the lane according to src !
- *       ! and road)                                      !                                                                 !
- *       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- *
- */
+ * Python. No ML / AI involved. */
 #define NDEBUG                 // Disable Boost debug mode
 #define BRIGHTNESS_L    100
 #define BRIGHTNESS_A    150
@@ -126,7 +111,6 @@ Mat drawArea(std::vector<int> recPoints, Mat& src);
 
 int main(int argc, char **argv)
 {
-    // TODO - Add a slider to adjust parameters 
     argsParser(argc, argv);
     if (im)
         processImage(filename, r, f);
@@ -159,7 +143,6 @@ Mat adaptBinary(Mat& img, int cond)
     // denoise image
     // Best thresholding method according to the conditions
     Mat binary;
-    // TODO - Best values ??
     adaptiveThreshold(img, binary, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 9, 2);
     // imshow("binary", binary);
     // waitKey();
@@ -186,7 +169,6 @@ Mat getTheROI(Mat& img, int fov)
 
     Point poly_points[1][3];
 
-    // TODO - Adjust fov precisely according to a type of road
     // Small FOV
     if (fov == 1) {
         poly_points[0][0] = Point((width * 1/8), height);
@@ -221,7 +203,6 @@ Mat getTheROI(Mat& img, int fov)
     bitwise_and(img, mask, dst);
 
     /* Display mask */
-    // TODO - add command line argument
     // imshow("roi", mask);
     // waitKey();
 
@@ -235,8 +216,8 @@ Mat getTheROI(Mat& img, int fov)
 Mat betterCanny(Mat& img, double median)
 {
     /* Benchmark */
-    double t;
-    t = (double)getTickCount();
+    //double t;
+    //t = (double)getTickCount();
     /* Get the best result from the Canny edge detector by detecting the ideal lower and upper
      * threshold values. */
     // A sigma of 0.33 seems to yield the best results.
@@ -251,7 +232,7 @@ Mat betterCanny(Mat& img, double median)
     // waitKey();
 
     /* Benchmark */
-    t = 1000*((double)getTickCount() - t)/getTickFrequency();
+    //t = 1000*((double)getTickCount() - t)/getTickFrequency();
     //std::cout << "betterCanny took\t" << t << " milliseconds to process the image." 
     //   << std::endl;
 
@@ -261,8 +242,8 @@ Mat betterCanny(Mat& img, double median)
 double medianMat(Mat img)
 {
     /* Benchmark */
-    double t;
-    t = (double)getTickCount();
+    //double t;
+    //t = (double)getTickCount();
     /* In order to get the optimal low and high threshold values for our Canny edge detector,
      * we need to compute the median pixel value of the image. */
     // Spread img to a single row
@@ -275,7 +256,7 @@ double medianMat(Mat img)
     int vn = v[n];
 
     /* Benchmark */
-    t = 1000*((double)getTickCount() - t)/getTickFrequency();
+    //t = 1000*((double)getTickCount() - t)/getTickFrequency();
     //std::cout << "medianMat took\t\t" << t << " milliseconds to process the image." 
     //   << std::endl;
 
@@ -289,8 +270,7 @@ double medianMat(Mat img)
 
 std::vector<Vec4i> HoughTransform(Mat& img)
 {
-    /* TODO  - Find the ideal parameters for Hough 
-     * dst: Output of the edge detector. It should be a grayscale image 
+    /* dst: Output of the edge detector. It should be a grayscale image 
      * (although in fact it is a binary one).
      * lines: A vector that will store the parameters (x_{start}, y_{start}, x_{end}, y_{end}) 
      * of the detected lines
@@ -303,8 +283,8 @@ std::vector<Vec4i> HoughTransform(Mat& img)
      */
 
     /* Benchmark */
-    double t;
-    t = (double)getTickCount();
+    //    double t;
+    //    t = (double)getTickCount();
 
     Mat houghed = img.clone();
 
@@ -312,7 +292,7 @@ std::vector<Vec4i> HoughTransform(Mat& img)
     std::vector<Vec4i> lines;
     HoughLinesP(img, lines, 4, CV_PI/180, 30, 40, 10);
 
-    t = 1000*((double)getTickCount() - t)/getTickFrequency();
+    //    t = 1000*((double)getTickCount() - t)/getTickFrequency();
     //std::cout << "Hough transform took\t" << t << " milliseconds to process the image." 
     //   << std::endl;
 
@@ -341,7 +321,6 @@ std::vector<int> getLinearFunction(std::vector<Vec4i> lines, int min_y, int max_
 
     double slope;
 
-    //TODO - Maybe find a better way to pass values to vectors
     for (size_t i = 0; i < lines.size(); i++) {
         /* If x2 - x1 == 0, continue */
         if ((lines[i][2] - lines[i][0]) == 0)
@@ -441,11 +420,11 @@ int processImage(std::string filename, bool render, int fov)
         std::cerr << "The image " << filename << " could not be loaded." << std::endl;
         return EXIT_FAILURE;
     }
-    //    std::cout << "Processing "<< filename << "..." << std::endl;
+        std::cout << "Processing "<< filename << "..." << std::endl;
 
     /* Benchmark */
-    double t;
-    t = (double)getTickCount();
+    //double t;
+    //t = (double)getTickCount();
 
     // 1. Frame pre-processing - common to any type of source
     // Determine brightness
@@ -481,7 +460,6 @@ int processImage(std::string filename, bool render, int fov)
     else if (BRIGHTNESS_A < brightness) {
         /* A median filter is applied to remove noise and reduce sharpness, 
          * improving the results of the edge detector. */
-        // TODO - find the best values for the bilateralFilter
         Mat smoothed = Mat::zeros(src_gray.size(), src_gray.type());
         bilateralFilter(src_gray, smoothed, 11, 75, 75, BORDER_DEFAULT);
         // imshow("Smoothed", smoothed);
@@ -495,10 +473,9 @@ int processImage(std::string filename, bool render, int fov)
         /* -> Before getting our ROI : we need to process the whole image to make the best
          * use of the canny edge detector
          * Canny operator is used. */
-        // TODO - Look for the best way to detect edges (Canny ?)
         edged = betterCanny(smoothed, median);
-        imshow("Edged", edged);
-        waitKey();
+        //imshow("Edged", edged);
+        //waitKey();
     }
 
     // b. ROI selection - common 
@@ -514,7 +491,6 @@ int processImage(std::string filename, bool render, int fov)
 
     // 2. Lane detection via Probabilistic Hough transform
     std::vector<Vec4i> lines = HoughTransform(ROI);
-    // TODO - ignore once and for all
 
     // 2. b - Generate one line per side
     double min_y, max_y;
@@ -548,16 +524,15 @@ int processImage(std::string filename, bool render, int fov)
 
 int processVideo(std::string filename, bool render, int fov)
 {
-    //TODO  - Handle video file error
     //      - Show average processing time per frame
     VideoCapture cap;
     cap.open(filename);
 
-    //std::cout << "Processing " << filename << "..." << std::endl;
+    std::cout << "Processing " << filename << "..." << std::endl;
 
     /* Benchmark */
-    double t;
-    t = (double)getTickCount();
+    //double t;
+    //t = (double)getTickCount();
     Mat src, src_gray, gray_denoised, smoothed, edged, ROI, denoised;
     for (;;) {
         cap >> src;
@@ -597,7 +572,6 @@ int processVideo(std::string filename, bool render, int fov)
         else if (BRIGHTNESS_A < brightness) {
             /* A median filter is applied to remove noise and reduce sharpness, 
              * improving the results of the edge detector. */
-            // TODO - find the best values for the bilateralFilter
             smoothed = Mat::zeros(src_gray.size(), src_gray.type());
             bilateralFilter(src_gray, smoothed, 5, 75, 75, BORDER_DEFAULT);
             // imshow("Smoothed", smoothed);
@@ -611,7 +585,6 @@ int processVideo(std::string filename, bool render, int fov)
             /* -> Before getting our ROI : we need to process the whole image to make the best
              * use of the canny edge detector
              * Canny operator is used. */
-            // TODO - Look for the best way to detect edges (Canny ?)
             edged = betterCanny(smoothed, median);
         }
 
@@ -624,7 +597,6 @@ int processVideo(std::string filename, bool render, int fov)
 
         // 2. Lane detection via Probabilistic Hough transform
         std::vector<Vec4i> lines = HoughTransform(ROI);
-        // TODO - ignore once and for all
 
         // 2. b - Generate one line per side
         double min_y, max_y;
